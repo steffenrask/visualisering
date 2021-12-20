70a361b5855d436a21acf41e64bdd3af7aae7dbf
steffenrask <62421614+steffenrask@users.noreply.github.com>
2021-12-20 14:15:28 +0100

    Add files via upload

--- dash_simple.py
+++ dash_simple.py
@@ -5,7 +5,7 @@ Created on Sat Dec 18 17:01:26 2021
 
 @author: steffenrask
 
-Dash simpel med slider for tid - så vi har en backup
+Dash simpel med dropdown for måneder
 
 Inspireret af volcanos
 
@@ -25,86 +25,89 @@ app.title = "Forest fires in Montesinho Park"
 
 ff_data = pd.read_csv('forestfires.csv', encoding='utf-8')
 
-time = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+# month_options = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
+# month_options.append("all")
+
+month_options = [
+    {'label': 'January', 'value': 'jan'},
+    {'label': 'February', 'value': 'feb'},
+    {'label': 'March', 'value': 'mar'},
+    {'label': 'April', 'value': 'apr'},
+    {'label': 'May', 'value': 'may'},
+    {'label': 'June', 'value': 'jun'},
+    {'label': 'July', 'value': 'jul'},
+    {'label': 'August', 'value': 'aug'},
+    {'label': 'September', 'value': 'sep'},
+    {'label': 'October', 'value': 'oct'},
+    {'label': 'November', 'value': 'nov'},
+    {'label': 'December', 'value': 'dec'}]
+month_options.append({'label': 'All Months', 'value': 'all'})
+
 
 app.layout = html.Div([
                  html.H1(children="Forest fires in Montesinho Park - Simple",
                          style = {'textAlign':'center', 'font-family' : 'Roboto'}),        
                  
                  html.Div([
-                     html.Div(
-                         className="visrow",
-                         children=[
-                             dcc.Graph(id='heatmap')
-                         ]),
-                     html.Div(
-                         className="visrow",
-                         children=[
-                             dcc.Graph(id='histogram')
-                         ]),
-                 ]),
+                     dcc.Checklist(id='checklist',
+                         options=month_options,
+                         value=['all'])
+                ]),
                  
+                 # html.Div([
+                 #     html.Div(
+                 #         className="visrow",
+                 #         children=[
+                 #             dcc.Graph(id='heatmap')
+                 #         ]),
                  html.Div([
-                     dcc.Slider(
-                       id='time-slider',
-                       min='all',
-                       max='dec',
-                       step=None,
-                       value=13,
-                       marks={
-                           'all': "All",
-                           'jan': "January",
-                           'feb': "February",
-                           'mar': "March",
-                           'apr': "April",
-                           'may': "May",
-                           'jun': "June",
-                           'jul': "July",
-                           'aug': "August",
-                           'sep': "September",
-                           'oct': "October",
-                           'nov': "November",
-                           'dec': "December"}
-                           )
-                 ])
+                     dcc.Graph(id='histogram')
+                 ]),
+                 # ])
 ])
                  
 
 @app.callback([
-     Output(component_id='heatmap', component_property='figure'),
+     #Output(component_id='heatmap', component_property='figure'),
      Output(component_id='histogram', component_property='figure')
      ],
     [
-#     Input(component_id='histogram', component_property='value'),
-     Input(component_id='time-slider', component_property='value')
+     Input(component_id='checklist', component_property='value')
     ]
 )
 
-def update_output(selected_month):
-    mydata = ff_data
-    if selected_month != 'all':
-        mydata = ff_data[ff_data['month'] == selected_month]
+def update_output(month_option):
     
-    count = mydata.groupby(['X', 'Y']).size().reset_index(name='fires').fillna(0)
-    fires=count.pivot_table(index='Y', columns='X', values='fires')
-    my_image = mpimg.imread("./forestfires.jpg")
-    heatmap = sns.heatmap(fires, alpha=0.7, zorder=3)
-    heatmap.imshow(my_image,
-             aspect=heatmap.get_aspect(),
-             extent= heatmap.get_xlim() + heatmap.get_ylim(),
-             zorder=1)
+    # Pick data for single month
+    # mydata = ff_data
+    # if month_option != 'all':
+    #     mydata = ff_data.loc[ff_data['month'] == month_option]
+    
+    # Heatmap
+    # count = mydata.groupby(['X', 'Y']).size().reset_index(name='fires').fillna(0)
+    # fires=count.pivot_table(index='Y', columns='X', values='fires')
+    # my_image = mpimg.imread("./forestfires.jpg")
+    # heatmap = sns.heatmap(fires, alpha=0.7, zorder=3)
+    # heatmap.imshow(my_image,
+    #          aspect=heatmap.get_aspect(),
+    #          extent= heatmap.get_xlim() + heatmap.get_ylim(),
+    #          zorder=1)
 
+    # Histogram
     histogram = px.histogram(
-        mydata, x="month", category_orders=dict(month=["jan", "feb", "mar", "apr",
+        ff_data, x="month", category_orders=dict(month=["jan", "feb", "mar", "apr",
                                                         "may", "jun", "jul", "aug",
                                                         "sep", "oct", "nov", "dec"]),
         range_y=[0,200]
     )
     histogram.update_xaxes(title_text='Month')
-    histogram.update_yaxes(title_text='')
+    histogram.update_yaxes(title_text='Fires')
 
 
-    return heatmap, histogram
+    return histogram
+
+
+# Her clickdata
 
 if __name__ == '__main__':
     app.run_server(debug=True, port=8081)
