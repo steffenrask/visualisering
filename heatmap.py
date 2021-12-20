584aba9bea99ca4123230b05722eb04e3b05fcb8
steffenrask <62421614+steffenrask@users.noreply.github.com>
2021-12-20 16:05:33 +0100

    Add files via upload

--- heatmap.py
+++ heatmap.py
@@ -7,10 +7,14 @@ import matplotlib.image as mpimg # add
 import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+
+from PIL import Image
+
 
 
 #%%
@@ -53,14 +57,38 @@ count = ff_data.groupby(['X', 'Y']).size().reset_index(name='fires').fillna(0)
 fires=count.pivot_table(index='Y', columns='X', values='fires')
 
 #load an image
-my_image = mpimg.imread("./forestfires.jpg")
+forestfires = Image.open("forestfires.jpg")
 
 h = px.imshow(fires)
 
-# update
-# h.imshow(my_image,
-#          aspect=h.get_aspect(),
-#          extent= h.get_xlim() + h.get_ylim(),
-#          zorder=1)
+h.update_layout(
+                images= [dict(
+                    source='forestfires',
+                    xref="paper", yref="paper",
+                    x=0, y=1,
+                    sizex=0.5, sizey=0.5,
+                    xanchor="left",
+                    yanchor="top",
+                    #sizing="stretch",
+                    layer="above")])
 
+h.show(renderer='chrome')
+
+
+#%% TAKE 3 MED GO
+
+ff_data = pd.read_csv('forestfires.csv', encoding='utf-8')
+count = ff_data.groupby(['X', 'Y']).size().reset_index(name='fires').fillna(0)
+fires=count.pivot_table(index='Y', columns='X', values='fires')
+
+heatmap = go.Heatmap(fires)
+
+forestfires = Image.open("forestfires.jpg")
+
+heatmap.add_layout_image(
+        dict(
+            source="forestfires",
+            xref="x",
+            yref="y",
+            layer="below"))       
 h.show(renderer='chrome')
